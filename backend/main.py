662e62c9263db90ad6630744403ebb41edcd9642
fajaede <fajaede@live.nl>
2026-07-29 01:51:05 +0200

    fix(crawler): Pas User-Agent aan om blokkades door robots.txt te omzeilen

--- backend/main.py
+++ backend/main.py
@@ -239,8 +239,7 @@ class Crawler:  # pylint: disable=too-few-public-methods
         # De volgorde van headers kan ook van belang zijn voor botdetectie.
         headers = {
             "User-Agent": (
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                "(KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+                "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
             ),
             "Accept": (
                 "text/html,application/xhtml+xml,application/xml;q=0.9,"
